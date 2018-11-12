84a6e1d6b167da55402c0681dfee78663fd2b82d
giangtx <36334907+giangtx@users.noreply.github.com>
2018-11-12 20:23:37 +0700

    test1

---
+++ laptrinh/vidu.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body >
+	<div class="container-fluid" id="div-top">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-6" id="hello-span">
+				<span >Chào mừng các bạn đến với website</span>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6" align="right">
+				<div class="p-t-logo">
+					<a class="p-r-logo" href="https://www.facebook.com/">
+						<img class="icon-logo" src="image/icon/fb-logo.png" alt="">
+					</a>
+					<a class="p-r-logo" href="https://www.google.com/">
+						<img class="icon-logo" src="image/icon/google-plus.png" alt="">
+					</a>
+					<a class="p-r-logo" href="">
+						<img class="icon-logo" src="image/icon/inta-logo.png" alt="">
+					</a>
+					<a class="p-r-logo" href="">
+						<img class="icon-logo" src="image/icon/sky-logo.png" alt="">
+					</a>
+					<a class="p-r-logo" href="">
+						<img class="icon-logo" src="image/icon/twitter-logo.png" alt="">
+					</a>
+					<a href="" id="login-top">Đăng nhập</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="padding-sly">
+					<h1 class="site-title">
+						<a href="#">Slytherin</a>
+					</h1>
+					<p class="site-tiny">Thời trang và phong cách</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="nav-sticky">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12">
+					<ul class="nav justify-content-center">
+					  <li class="nav-item">
+					    <a class="nav-link active" href="#">Trang chủ</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Cửa hàng</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Sản phẩm hot</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Thông tin</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Liên kết</a>
+					  </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="new-arrivals">
+		<div id="new-arrivals-text">
+			<span style="font-size: 25px">Bộ sưu tập cho phái nữ 2018</span><br>
+			<span style="font-size:80px ;font-weight: 500">XU THẾ MỚI</span><br>
+			<button>Tham gia ngay</button>
+		</div>
+	</div>
+	<div class="boder">
+		<div class="container" id="info">
+			<div class="row">
+				<div class="col-sm-4 col-md-4 ">
+					<img class="icon-info" alt="huychuong" src="image/ribbon-badge-award.png"/><br>
+					<span class="f-40" >CHẤT LƯỢNG</span><br>
+					<span>Chúng tôi cam kết mang đến cho bạn chất lượng sản phẩm đạt chuẩn nhất</span>
+				</div>
+				<div class="col-sm-4 col-md-4 ">
+					<img class="icon-info" alt="tien" src="image/different-currencies.png"/><br>
+					<span class="f-40">TIẾT KIỆM CHI PHÍ</span><br>
+					<span>Cam kết giá rẻ nhất cả nước giúp các bạn tiết kiệm hơn 90% cho từng sản phẩm</span>
+				</div>
+				<div class="col-sm-4 col-md-4 ">
+					<img class="icon-info" alt="Giaodich" src="image/handshake.png"/><br>
+					<span class="f-40">GIAO HÀNG</span><br>
+					<span>Cam kết giao hàng tận nơi trong ngày. Để mang sản phẩm đến cho khách hàng nhanh nhất</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="product">
+		<div class="container">
+			<span class="f-40">DANH MỤC</span>
+			<div class="row">
+				<div class="col-lg-4 col-md-12 col-sm-12 m-l-r-auto">
+					<div class="m-t-b pos-relative">
+						<img class="image-banner-top" src="image/banner-02.jpg" alt="">
+						<div class="w-size block-btn">
+							<a class="align-item" href="" title="">ĐẦM</a>
+						</div>
+					</div>
+					<div class="m-t-b pos-relative">
+						<img class="image-banner-bottom" src="image/banner-05.jpg" alt="">
+						<div class="w-size block-btn">
+							<a class="align-item" href="" title="">KÍNH RÂM</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-12 col-sm-12 pos-relative">
+					<div class="m-t-b">
+						<img class="image-banner-bottom" src="image/banner-03.jpg" alt="">
+						<div class="w-size block-btn" style="bottom: 560px;">
+							<a class="align-item" href="" title="">ĐỒNG HỒ</a>
+						</div>
+					</div>
+					<div class="m-t-b pos-relative">
+						<img class="image-banner-top" src="image/banner-07.jpg" alt="">
+						<div class="w-size block-btn">
+							<a class="align-item" href="" title="">GIÀY</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-12 col-sm-12 pos-relative">
+					<div class="m-t-b">
+						<img class="image-banner-top" src="image/banner-04.jpg" alt="">
+						<div class="w-size block-btn" style="bottom: 421px;">
+							<a class="align-item" href="" title="">BALO</a>
+						</div>
+					</div>
+					<div class="image-banner-bottom" id="sale-banner">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="info-bottom">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="padding-sly">
+						<h1 class="site-title">
+							<a href="#">Slytherin</a>
+						</h1>
+						<p class="site-tiny">Thời trang và phong cách</p>
+					</div>
+				</div>
+				<div class="col-lg-8">
+					<div class="row">
+						<div class="col-lg-6">
+							<span class="font-bold-bottom">Liên hệ với chúng tôi</span><br><br>
+							<div class="font-info-bottom">
+								<span>Bạn có câu hỏi gì không?</span><br>
+								<span>Hãy cho chúng tôi biết tại cửa hàng ở 12 Tây Sơn Hà Nội hoặc liên lạc với chúng tôi qua số điện thoại 099099099</span><br>
+							</div>
+							<div class="p-t-logo">
+								<a class="p-r-logo" href="https://www.facebook.com/">
+									<img class="icon-logo" src="image/icon/fb-logo.png" alt="">
+								</a>
+								<a class="p-r-logo" href="https://www.google.com/">
+									<img class="icon-logo" src="image/icon/google-plus.png" alt="">
+								</a>
+								<a class="p-r-logo" href="">
+									<img class="icon-logo" src="image/icon/inta-logo.png" alt="">
+								</a>
+								<a class="p-r-logo" href="">
+									<img class="icon-logo" src="image/icon/sky-logo.png" alt="">
+								</a>
+								<a class="p-r-logo" href="">
+									<img class="icon-logo" src="image/icon/twitter-logo.png" alt="">
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<span class="font-bold-bottom">Danh mục</span><br><br>
+							<div >
+								<ul >
+									<li class="p-b-li"><a href="#" class="font-info-bottom ">Đầm</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Kính râm</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Đồng hồ</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Giày</a></li>
+								</ul>
+
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<span class="font-bold-bottom">Liên kết</span><br><br>
+							<div >
+								<ul >
+									<li class="p-b-li"><a href="#" class="font-info-bottom ">Tìm kiếm</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Về chúng tôi</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Liên hệ chúng tôi</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Facebock</a></li>
+								</ul>
+
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-4">
+							<span class="font-bold-bottom">Trợ giúp</span><br><br>
+							<div >
+								<ul >
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Kiểm tra</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Giỏ hàng</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Sản phẩm</a></li>
+									<li class="p-b-li"><a href="#" class="font-info-bottom">Giao dịch</a></li>
+								</ul>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="p-l-r-bt">
+			<div class="row">
+				<div class="col-lg-6">
+					<span class="font-info-bottom">© 2018 Slytherin. Tất cả các quyền được bảo lưu.</span>
+				</div>
+				<div class="col-lg-6">
+					<span class="font-info-bottom">Khu vực và các quốc gia: Việt Nam, Thái Lan, Singapore, Lào, Nhật Bản</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+	<script src="JS/customs.js"></script>
+</body>
+</html>
